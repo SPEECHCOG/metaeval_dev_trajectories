a772b351def30be84f6ae4cfdf0ce2f610f41f57
andrea08 <andrea1391@gmail.com>
2022-03-11 01:44:01 +0200

    plots per capability overlapped with infants' data

--- r_scripts/visualise_capabilities.R
+++ r_scripts/visualise_capabilities.R
@@ -52,23 +52,21 @@ get_overlapped_plot <- function(model_effects, title){
 }
 
 get_overlapped_plot_per_cap <- function(models_effects, cap){
+  
   infants_data = all_capabilities_infants %>% filter(capability==cap)
   models_data = models_effects %>% filter(capability==cap)
-
+  
+  colours_plot = c("APC" = "#7629A0", "CPC" = "#E27000")
+  labels_plot = c("APC", "CPC")
+  
   overlapped_plot <- ggplot(infants_data, aes(x = days, y = ds)) + 
-    #scale_color_manual(values = c("IDS preference" = "#00BA38",
-    #                              "Native discrimination" = "#F8766D",
-    #                              "Non-native discrimination" = "#619CFF")) +
-    #scale_fill_manual(values = c("IDS preference" = "#00BA38",
-    #                             "Native discrimination" = "#F8766D",
-    #                             "Non-native discrimination" = "#619CFF")) +
     geom_hline(yintercept = 0, linetype = "dashed", color = "grey") +
     
     scale_size_continuous(guide = "none") +
     scale_y_continuous(expand = c(0, 0), limits = c(-1, 4.5),
                        breaks = seq(-1, 4.5, 0.5)) +
     coord_cartesian(clip = "off") +
-    xlab("\nMean age (days)") +
+    xlab("\nSimulated age (days)") +
     ylab("Effect Size\n") + 
     
     geom_point(size=1.5, data=models_data, aes(y=d, x=days, 
@@ -95,25 +93,37 @@ get_overlapped_plot_per_cap <- function(models_effects, cap){
                                                      colour="#00BA38",
                                                      fill="#00BA38",
                                                      linetype="dotted")
+    colours_plot <- c(colours_plot, "IDS preference" = "#00BA38")
+    labels_plot <- c(labels_plot, "Infants")
+    
   }else{
     # Vowel discrimination
     overlapped_plot <- overlapped_plot + 
-      annotate('ribbon', x = c(1, 166), ymin = mean_es_nat_ci.lb, 
-               ymax = mean_es_nat_ci.ub, 
-               alpha = 0.5, fill=alpha("#F8766D", alpha=0.2)) +
-      annotate('ribbon', x = c(1, 166), ymin = mean_es_nonnat_ci.lb, 
-               ymax = mean_es_nonnat_ci.ub, 
-               alpha = 0.5, fill=alpha("#619CFF", alpha=0.8)) + 
       geom_line(size=0.9, linetype="dotted", aes(x = days, y = ds,
-                                                 colour=capability), data=infants_data) +
-      scale_color_manual(values = c("Native discrimination" = "#F8766D",
-                                    "Non-native discrimination" = "#619CFF",
-                                    
-                                    "APC" = "#C4A0D9",
-                                    "NCPC" = "#FFAF49",
-                                    "Non-native discrimination.APC" = "#7629A0",
-                                    "Non-native discrimination.CPC" = "#E27000"))
+                                                 colour=capability), data=infants_data)
+    if(cap=="Native discrimination"){
+      overlapped_plot <- overlapped_plot +
+        annotate('ribbon', x = c(1, 166), ymin = mean_es_nat_ci.lb, 
+                 ymax = mean_es_nat_ci.ub, 
+                 alpha = 0.5, fill=alpha("#F8766D", alpha=0.2))
+      colours_plot <- c(colours_plot, "Native discrimination" = "#F8766D")
+      labels_plot <- c(labels_plot, "Infants")
+    }else{
+      overlapped_plot <- overlapped_plot +
+        annotate('ribbon', x = c(1, 166), ymin = mean_es_nonnat_ci.lb, 
+                 ymax = mean_es_nonnat_ci.ub, 
+                 alpha = 0.5, fill=alpha("#619CFF", alpha=0.8))
+      
+      colours_plot <- c(colours_plot, "Non-native discrimination" = "#619CFF")
+      labels_plot <- c(labels_plot, "Infants")
+    }
   }
+  
+  # Colouring 
+  overlapped_plot <- overlapped_plot + 
+    scale_color_manual(values = colours_plot, labels=labels_plot)+
+    scale_fill_manual(values = colours_plot, labels=labels_plot)
+  
   return(overlapped_plot)
 }
 
@@ -153,7 +163,7 @@ cpc_results$predicted = 0
 
 for(model in models){
   for(capability_name in c("IDS preference", "Native discrimination", "Non-native discrimination")){
-    print(capability_name)
+    
     if(model=="apc"){
       subset = apc_results %>% filter(capability==capability_name)
     }else{
@@ -181,9 +191,6 @@ for(model in models){
 
 apc_results$model = "APC"
 cpc_results$model = "CPC"
-
-
-
 
 # Plot effect sizes
 plot_traj <- create_plot_trajectories(apc_results, "APC")
