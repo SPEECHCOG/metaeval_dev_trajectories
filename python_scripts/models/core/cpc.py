8508dcf9336eb0a9948740abf4454f8fc3b884d7
orasanen <okko.rasanen@tuni.fi>
2022-09-27 10:13:15 +0300

    CPU support

--- python_scripts/models/core/cpc.py
+++ python_scripts/models/core/cpc.py
@@ -14,7 +14,11 @@ from core.model_base import ModelBase
 from core.utils import FeatureEncoder, ContrastiveLoss
 
 physical_devices = tf.config.list_physical_devices('GPU')
-tf.config.experimental.set_memory_growth(physical_devices[0], enable=True)
+
+
+if(len(physical_devices) > 0):
+    tf.config.experimental.set_memory_growth(physical_devices[0], enable=True)
+
 
 
 class CPCModel(ModelBase):
